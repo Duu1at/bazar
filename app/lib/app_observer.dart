d801f9b815276c0f8e780f26742a29cad871236f
Duu1at <dbolsunbekuulu@gmail.com>
2025-01-28 18:02:09 +0600

    fix auth bloc

--- app/lib/app_observer.dart
+++ app/lib/app_observer.dart
@@ -7,31 +7,26 @@ class AppBlocObserver extends BlocObserver {
 
   @override
   void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
-    // onLog?.call('onEvent bloc: $bloc - event: $event');
     super.onEvent(bloc, event);
   }
 
   @override
   void onCreate(BlocBase<dynamic> bloc) {
     super.onCreate(bloc);
-    // onLog?.call('onCreate(${bloc.state})');
   }
 
   @override
   void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
     super.onChange(bloc, change);
-    // onLog?.call('onChange(${bloc.runtimeType}, $change)');
   }
 
   @override
   void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    // onLog?.call('onError(${bloc.runtimeType}, $error, $stackTrace)');
     super.onError(bloc, error, stackTrace);
   }
 
   @override
   void onClose(BlocBase<dynamic> bloc) {
     super.onClose(bloc);
-    // onLog?.call('onClose(${bloc.runtimeType})');
   }
 }
